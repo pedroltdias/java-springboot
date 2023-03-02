9afbffd38a3505a1e0c96a38a801c9b50bc8415b
Pedro Dias <pedroluksteixeira@gmail.com>
2023-03-02 15:29:53 -0300

    Fix: Optimizing the pagination sistem and setting default options

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -33,9 +34,9 @@ public class TopicosController {
     @GetMapping
     @ResponseBody
     public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso,
-                                 @RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao){
+                                 @PageableDefault(sort = "id", direction = Sort.Direction.ASC, page = 0, size = 10) Pageable paginacao){
 
-        Pageable paginacao = PageRequest.of(pagina, qtd, Sort.Direction.DESC, ordenacao);
+
 
         if(nomeCurso == null){
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
