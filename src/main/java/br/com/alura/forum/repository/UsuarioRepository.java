fcfc0c816f3a5f2187d81faa924c67177c9a59fb
Pedro Dias <pedroluksteixeira@gmail.com>
2023-03-07 16:12:31 -0300

    Feat: Implementing auth service, user repository and security configs

---
+++ src/main/java/br/com/alura/forum/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findByEmail(String email);
+
+}
