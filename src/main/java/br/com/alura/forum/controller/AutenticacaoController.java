fca4ea336caab9843523790f162dbdc6c9afe7c5
Pedro Dias <pedroluksteixeira@gmail.com>
2023-03-08 16:51:16 -0300

    Feat: 'AutenticacaoController' for auth and LoginFormDTO

---
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -0,0 +1,26 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.controller.form.LoginFormDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController {
+
+    @PostMapping
+    public ResponseEntity<?> autenticar(@RequestParam @Valid LoginFormDTO form){
+        System.out.println(form.getEmail());
+        System.out.println(form.getSenha());
+
+
+
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/br/com/alura/forum/controller/form/LoginFormDTO.java
@@ -0,0 +1,23 @@
+package br.com.alura.forum.controller.form;
+
+public class LoginFormDTO {
+
+    private String email;
+    private String senha;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
